370390f7d862252bd3cfff2126be0df54f322afd
jitendra saraf <156499410+jiten398@users.noreply.github.com>
2024-12-04 17:08:31 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import UserForm from './UserForm';
 import VoiceCodeGenerator from './Agent';
 import { supabase } from './supabase';
